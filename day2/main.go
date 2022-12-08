d902bc3e983cba018e2cc6ecd6bb3725b282bb82
pruetj <james_pruet@oxy.com>
2022-12-08 16:34:06 -0600

    onto day 4

--- day2/main.go
+++ day2/main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Round struct {
+	Opp string
+	Me  string
+}
+
+var rounds = make([]Round, 0)
+
+type Score map[Round]int
+
+var scores = make(Score)
+var scoresP2 = make(Score)
 
 func main() {
-	fmt.Println("hi")
+	loadInput()
+	Question1()
+	Question2()
+}
+
+func loadInput() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		splitText := strings.Split(scanner.Text(), " ")
+		rounds = append(rounds, Round{splitText[0], splitText[1]})
+	}
+
+	scores[Round{"A", "X"}] = 4
+	scores[Round{"B", "X"}] = 1
+	scores[Round{"C", "X"}] = 7
+	scores[Round{"A", "Y"}] = 8
+	scores[Round{"B", "Y"}] = 5
+	scores[Round{"C", "Y"}] = 2
+	scores[Round{"A", "Z"}] = 3
+	scores[Round{"B", "Z"}] = 9
+	scores[Round{"C", "Z"}] = 6
+
+	scoresP2[Round{"A", "X"}] = 3
+	scoresP2[Round{"B", "X"}] = 1
+	scoresP2[Round{"C", "X"}] = 2
+	scoresP2[Round{"A", "Y"}] = 4
+	scoresP2[Round{"B", "Y"}] = 5
+	scoresP2[Round{"C", "Y"}] = 6
+	scoresP2[Round{"A", "Z"}] = 8
+	scoresP2[Round{"B", "Z"}] = 9
+	scoresP2[Round{"C", "Z"}] = 7
+
+}
+
+func Question1() {
+	score := 0
+	for _, v := range rounds {
+		score += scores[v]
+	}
+
+	fmt.Println("Question 1: ", score)
+}
+
+func Question2() {
+	score := 0
+	for _, v := range rounds {
+		score += scoresP2[v]
+	}
+
+	fmt.Println("Question 2: ", score)
 }
---
+++ day3/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Compartment struct {
+	Left  string
+	Right string
+}
+
+type Group struct {
+	e1 string
+	e2 string
+	e3 string
+}
+
+var list = make([]Compartment, 0)
+var listQ2 = make([]Group, 0)
+
+var badVals = make([]string, 0)
+var prioSum int
+var badgeSum int
+
+func main() {
+	loadInput()
+	Question1()
+	Question2()
+}
+
+func loadInput() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	counter := 0
+	g := Group{}
+	for scanner.Scan() {
+		s := scanner.Text()
+		list = append(list, Compartment{s[:len(s)/2], s[len(s)/2:]})
+
+		switch num := counter % 3; num {
+		case 0:
+			g.e1 = s
+		case 1:
+			g.e2 = s
+		case 2:
+			g.e3 = s
+			listQ2 = append(listQ2, g)
+			g = Group{}
+		}
+		counter += 1
+	}
+}
+
+func Question1() {
+	for _, v := range list {
+		check := make(map[string]bool)
+		for _, v := range v.Left {
+			check[string(v)] = true
+		}
+
+		for _, v := range v.Right {
+			if _, ok := check[string(v)]; ok {
+				if casing := int(v); casing > 90 {
+					prioSum += casing - 96
+				} else {
+					prioSum += casing - 38
+				}
+				badVals = append(badVals, string(v))
+				break
+			}
+		}
+	}
+
+	fmt.Println("Question 1: ", prioSum)
+
+}
+
+func Question2() {
+	for _, v := range listQ2 {
+		check := make(map[string]int)
+		for _, v := range v.e1 {
+			check[string(v)] = 1
+		}
+
+		for _, v := range v.e2 {
+			if val := check[string(v)]; val == 1 {
+				check[string(v)] = 2
+			}
+		}
+
+		for _, v := range v.e3 {
+			if val := check[string(v)]; val == 2 {
+				check[string(v)] = 3
+			}
+		}
+
+		for k, v := range check {
+			if v != 3 {
+				delete(check, k)
+			}
+		}
+
+		for k, _ := range check {
+			if int(k[0]) > 90 {
+				badgeSum += int(k[0]) - 96
+			} else {
+				badgeSum += int(k[0]) - 38
+			}
+		}
+	}
+	fmt.Println("Question 2: ", badgeSum)
+}
